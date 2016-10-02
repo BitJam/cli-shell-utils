bd042c757a558baca3be50c58907ce3534850085
James Bowlin <bitjam@gmail.com>
2016-10-01 23:00:03 -0600

    Set lab_co to nc_co when --color=low

--- cli-shell-utils.bash
+++ cli-shell-utils.bash
@@ -1359,7 +1359,7 @@ set_colors() {
 
         from_co=$brown ;       bold_co=$white ;    dev_co=$white ;
           hi_co=$white ;    version_co=$white ;  quest_co=$green ;
-           m_co=$nc_co ;      fname_co=$nc_co ;
+           m_co=$lt_gray ;    fname_co=$nc_co ;    lab_co=$nc_co ;
          num_co=$white ;      date_co=$nc_co  ;
     fi
 }
