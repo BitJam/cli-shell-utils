87398f6e5266983c211ec92327dbac84184b4fd5
James Bowlin <bitjam@gmail.com>
2017-06-03 13:03:37 -0600

    Add convenient menu_printf() routine

--- cli-shell-utils.bash
+++ cli-shell-utils.bash
@@ -442,6 +442,14 @@ YES_no_pretend() {
 shout_pretend() { [ "$PRETEND_MODE" ] && Shout $"PRETEND MODE ENABLED" ;}
 
 #------------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------------
+menu_printf() {
+    local payload=$1  fmt=$2  ; shift 2
+    printf "%s$P_IFS$fmt\n" "$payload" "$@"
+}
+
+#------------------------------------------------------------------------------
 # Generate a simple selection menu based on a data:label data structure.
 # The "1)" and so on get added automatically.
 #------------------------------------------------------------------------------
