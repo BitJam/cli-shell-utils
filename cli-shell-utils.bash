627cda1598891b3fb4d42a64453c53428ac8b0f0
James Bowlin <bitjam@gmail.com>
2019-02-27 20:20:56 -0700

    Bump version

--- cli-shell-utils.bash
+++ cli-shell-utils.bash
@@ -19,8 +19,8 @@
 #==============================================================================
 
 LIB_NAME="cli-shell-utils"
-LIB_VERSION="2.20.02"
-LIB_DATE="Sun Feb 24 14:14:50 MST 2019"
+LIB_VERSION="2.20.03"
+LIB_DATE="Wed Feb 27 20:20:50 MST 2019"
 
 : ${ME:=${0##*/}}
 : ${MY_DIR:=$(dirname "$(readlink -f $0)")}
